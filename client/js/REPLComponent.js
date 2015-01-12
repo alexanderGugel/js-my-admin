586e8ec9c0e1f2065bd948fb71965c0d8ed9fe9a
Alexander Gugel <alexander.gugel@gmail.com>
2015-01-13 00:53:51 +0100

    More features and cleanup

--- client/js/REPLComponent.js
+++ client/js/REPLComponent.js
@@ -1,10 +1,17 @@
 var ResultTableComponent = require('./ResultTableComponent');
 var React                = require('react');
+var query                = require('./query');
 
 var REPLComponent = React.createClass({
   getInitialState: function() {
     return {
-      sqlQuery: this.props.sqlQuery
+      sqlQuery: this.props.sqlQuery,
+      schemasSelection: {
+        display: 'none'
+      },
+      schemasList: [],
+      schemasMap: {},
+      schemasLoaded: false
     };
   },
   handleChange: function(event) {
@@ -27,11 +34,37 @@ var REPLComponent = React.createClass({
     this.props.router.navigate('/repl/' + encodeURIComponent(this.state.sqlQuery), { trigger: true });
   },
   handleSchemasClick: function(event) {
+    var rect = event.target.getBoundingClientRect();
+    this.setState({
+      schemasSelection: {
+        display: this.state.schemasSelection.display === 'none' ? 'inline-block' : 'none',
+        top: rect.bottom + 'px',
+        left: rect.left + 'px'
+      }
+    });
 
+    if (!this.state.schemasLoaded) {
+      query('SELECT * FROM information_schema.tables;', localStorage.getItem('connectionString'), function(error, result) {
+        console.log(result.rows[0])
+        var schemasMap = {};
+        result.rows.forEach(function(row) {
+          schemasMap[row.table_schema] = schemasMap[row.table_schema] || [];
+          schemasMap[row.table_schema].push(row.table_name);
+        });
+        var schemasList = Object.keys(schemasMap);
+        this.setState({schemasList: schemasList, schemasMap: schemasMap});
+      }.bind(this));
+    }
   },
   render: function() {
     return (
       <section className="repl">
+        <ul className="schema-selection" style={this.state.schemasSelection}>
+          <li className="section"><strong>Schemas</strong></li>
+          {this.state.schemasList.map(function(schema) {
+            return <li key={schema}>{schema}</li>;
+          })}
+        </ul>
         <nav className="row">
           <div className="col-md-4 schemas">
             <button onClick={this.handleSchemasClick}>Schemas</button>
